bbec2ac7e2d453958ce0cac89cb9b963956e9f4e
gyi <ginay2521@gmail.com>
2018-11-15 17:19:22 -0500

    changed connection

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 5001;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
-//mongoose.connect('mongodb://gyi:NamJee01@ds121282.mlab.com:21282/heroku_js7qh2wp', {useNewUrlParser: true});
-mongoose.connect('mongodb://localhost/toDoWithSocket', {useNewUrlParser: true});
+mongoose.connect('mongodb://gyi:NamJee01@ds121282.mlab.com:21282/heroku_js7qh2wp', {useNewUrlParser: true});
+//mongoose.connect('mongodb://localhost/toDoWithSocket', {useNewUrlParser: true});
 
 require('./sockets/todo-sockets')(io);
 require('./routes/api-routes')(app);
