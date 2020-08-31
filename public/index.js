d42ef28809494ace38b365d80327415b5f8373bf
gyi <ginay2521@gmail.com>
2020-08-31 18:06:23 -0400

    made some changes on index and todo-sockets page

--- public/index.js
+++ public/index.js
@@ -13,52 +13,54 @@ $(function () {
     const month = moment().format('MMM D');
     const year = moment().format('YYYY');
 
-    render(`<div class="day">${day}</div><div><div class="month">${month}</div><div class="year">${year}</div></div>` );
+    render(`<div class="day">${day}</div><div><div class="month">${month}</div><div class="year">${year}</div></div>`);
 
-    $frmSendToDo.submit(function(event){
+    $frmSendToDo.submit(function (event) {
         event.preventDefault();
         socket.emit('send-task', $inputTask.val())
         $inputTask.val('');
     })
 
-    socket.on('load old msg', function(docs){
-        for(let i=0; i<docs.length; i++){
-            displayMsg(`<p class="uppercase"><span ${docs[i].done? 'class="grayout"':'class=""'}> ${docs[i].task} </span><button id="${docs[i]._id}" class="remove"><i ${docs[i].done? 'class="far fa-times-circle btn-style iBtn"':'class = "far fa-circle btn-style"'}></i></button></p>`);
+    socket.on('load old msg', function (docs) {
+        for (let i = 0; i < docs.length; i++) {
+            displayMsg(`<p class="uppercase"><span ${docs[i].done ? 'class="grayout"' : 'class=""'}> ${docs[i].task} </span><button id="${docs[i]._id}" class="remove"><i ${docs[i].done ? 'class="far fa-times-circle btn-style iBtn"' : 'class = "far fa-circle btn-style"'}></i></button></p>`);
         }
     })
 
-    socket.on('new-task', function(data){
-        displayMsg(`<p class="uppercase"><span> ${data.task} </span><button id="${data._id}" class="remove"><i ${data.done? 'class="far fa-times-circle btn-style"':'class = "far fa-circle btn-style"'}></i></button></p>`);
+    socket.on('new-task', function (data) {
+        displayMsg(`<p class="uppercase"><span> ${data.task} </span><button id="${data._id}" class="remove"><i ${data.done ? 'class="far fa-times-circle btn-style"' : 'class = "far fa-circle btn-style"'}></i></button></p>`);
     })
 
-    socket.on('update-task', function(data){
-        let updatedTask =  $("#" + data._id).parent();
+    socket.on('update-task', function (data) {
+        let updatedTask = $("#" + data._id).parent();
         $(updatedTask).find('span').addClass('grayout');
         let chk = $(updatedTask).find('.fa-circle');
         chk.removeClass('fa-circle');
         chk.addClass('fa-times-circle iBtn');
     })
 
-    socket.on('delete-task', function(data){
+    socket.on('delete-task', function (data) {
         $("#" + data._id).parent().remove();
-     })
+    })
 
-    $(document).on('click', '.remove', function(){
-        let taskId= $(this).attr("id");
+    $(document).on('click', '.remove', function () {
+        let taskId = $(this).attr("id");
         let chk = $(this).find('.fa-circle');
-        if(chk.length > 0){
+        if (chk.length > 0) {
             chk.removeClass('fa-circle');
             chk.addClass('fa-times-circle iBtn');
             $(this).closest('p').find('span').addClass('grayout');
             socket.emit('complete-task', taskId)
         }
-        else{           
+        else {
             $(this).parent().remove();
-            socket.emit('delete-task', taskId);  
+            socket.emit('delete-task', taskId);
         }
     })
-    function displayMsg(data){
-        $toDoList.append(data );
+
+
+    function displayMsg(data) {
+        $toDoList.append(data);
     }
 })
 
--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const moment = require('moment');
+// const moment = require('moment');
 const PORT = process.env.PORT || 5001;
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-mongoose.connect('mongodb://gyi:NamJee01@ds121282.mlab.com:21282/heroku_js7qh2wp', {useNewUrlParser: true});
+//mongoose.connect('mongodb://gyi:NamJee01@ds121282.mlab.com:21282/heroku_js7qh2wp', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://gina2521:NamJee2521@cluster-todosocket.qd1ot.mongodb.net/heroku_js7qh2wp?retryWrites=true&w=majority')
 //mongoose.connect('mongodb://localhost/toDoWithSocket', {useNewUrlParser: true});
 
 require('./sockets/todo-sockets')(io);
